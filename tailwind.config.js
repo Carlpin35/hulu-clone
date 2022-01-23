3b0253f743d96a577bec210b67fb73fbd15128ce
Carlpin35 <carlpin800@gmail.com>
2022-01-23 17:50:05 -0300

    3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx", "./public/**/*.html"]
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
